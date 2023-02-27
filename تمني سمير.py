4f3bf90252be901cbf0ed2d4be7812d900466446
EbraKhrbi <126418603+EbraKhrbi@users.noreply.github.com>
2023-02-28 01:17:53 +0300

    Add files via upload

---
+++ تمني سمير.py
@@ -0,0 +1,92 @@
+# # ### لتكليف التي سوفا يتم مناقشتها من قبل الدكتور
+# # ###التكليف الاول التعرف ع نوع الداله
+x='tamani'
+print(type(x))
+# ##################
+# # ##التكليف الثاني ,طباعة الارقام الموضحة ف لست  
+# k=[1,2,3,3,3,3,4,5]
+# print(set(k))
+# # ##طريقه ثانيه لطباعةالعداد بغير تكرار و مع ترتيب *_*ع
+# k=[1,2,3,3,3,3,4,5]
+# list(dict.fromkeys(k))
+# ##############قم بعمل برنامج يطبع العدد من الجهتين  مع طباعة صح او غلط
+# y=input("enter the streng")
+# a=""
+# b=len(y)
+# for i in y:
+#   a+=y[b-1]
+#   b=b-1
+# d=a
+# print(d)
+# if d==y:
+#   print(True)
+
+
+# else:
+#   print(Fals)
+# ############  برنامج لحساب المساحه لدائره 
+# class cl:
+#   def _inti_(self,p,a):
+#      self.p =3.14159
+#      self.a=a
+#   def o(self):
+#      w=self.p*self.a*self.a*self.a
+#      print(w)
+
+
+# obj1=cl(p,44.4)
+# obj1.o()
+#   ###################
+# # ####التكاليف التي لم يتم مناقشتها*^*
+#  a=[53,32,19,79,25,5,47]
+#  max=a[0]
+#  min=a[0]
+#  d=0
+#  for i in a:
+#   if("max=",max,"min=",min)
+# ############################
+#  a=int(input("enter1"))
+#  b=int(input("enter2"))
+#  c=a|b
+#  if a%b==0:
+#   print(True)
+#  if a%b==1:
+#    print(False)
+#  print("******************")
+#  a=int(input("enter num"))
+#  r=[1,5,8,3,3]
+#  if a in r:
+# #    print(True)
+# #  else:
+# #    print(False)
+# #  print("******************")
+# #  num=[386,462,47,418,907,344,236,6,8, 237,9,9,0,4.28,7,2,8,1,9]
+# #  for i in num :
+# #    if i%2==0:
+# #      print(i)
+# #    if i==237:
+# #        break
+# # ##3عمل برنامج يطبع ضروب العدد 
+# #    
+# k=int(input("enter"))
+# xx=1
+# for i in range(1,k+1):
+#     xx=xx*i
+#     print(xx)
+# print("*****************************")
+# ####برنامج يقوم بحساب مساحة ومحيط المربع
+# class rectangle:
+#     def __init__(self,length,width):
+#         self.length=length
+#         self.width=width
+#     def area_rectangle(self,length,width):
+#         area=length*width
+#         return area
+#     def mference_rectangle(self,length,width):
+#         mference1=2*(length+width)
+#         return mference1
+#     x=int(input("enter len"))
+#     y=int(input("enter wid"))
+#     rectangle1=rectangle (x,y)
+#     print(rectangle1.area_rectangle(x,y))
+#     print(rectangle1.mference_rectangle(x,y))
